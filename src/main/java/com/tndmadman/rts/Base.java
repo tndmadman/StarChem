b963e0ed1f8383830d842d39046adcdb66d9c397
Alic Kutia <tjmacclain98@gmail.com>
2026-07-03 02:59:11 -0600

    Render base owner colors and names

--- src/main/java/com/tndmadman/rts/Base.java
+++ src/main/java/com/tndmadman/rts/Base.java
@@ -19,14 +19,16 @@ final class Base {
         return Calc.distance(wx, wy, x, y) <= (typeId.equals("shipyard") ? 82 : 64);
     }
 
-    void draw(Graphics2D g2, Color playerColor, EnumMap<Material, Double> stockpile, boolean local) {
+    void draw(Graphics2D g2, Color ignoredColor, EnumMap<Material, Double> stockpile, boolean ignoredLocal) {
+        Color playerColor = PlayerRegistry.color(playerId);
+        boolean local = PlayerRegistry.isLocal(playerId);
         Graphics2D s = (Graphics2D) g2.create();
         s.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         BaseType def = type();
         double radius = typeId.equals("shipyard") ? 82 : 64;
-        s.setColor(new Color(playerColor.getRed(), playerColor.getGreen(), playerColor.getBlue(), local ? 42 : 25));
+        s.setColor(new Color(playerColor.getRed(), playerColor.getGreen(), playerColor.getBlue(), local ? 42 : 22));
         s.fillOval((int)(x - def.unloadRange), (int)(y - def.unloadRange), (int)(def.unloadRange * 2), (int)(def.unloadRange * 2));
-        s.setColor(new Color(playerColor.getRed(), playerColor.getGreen(), playerColor.getBlue(), local ? 110 : 70));
+        s.setColor(new Color(playerColor.getRed(), playerColor.getGreen(), playerColor.getBlue(), local ? 120 : 72));
         s.setStroke(new BasicStroke(1.4f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 0, new float[]{10f,8f}, 0));
         s.drawOval((int)(x - def.unloadRange), (int)(y - def.unloadRange), (int)(def.unloadRange * 2), (int)(def.unloadRange * 2));
         Polygon hull = new Polygon();
@@ -38,11 +40,11 @@ final class Base {
         s.setColor(playerColor); s.setStroke(new BasicStroke(3f)); s.drawPolygon(hull);
         s.setColor(new Color(125,205,255,90)); s.fillOval((int)(x - 26), (int)(y - 26), 52, 52);
         s.setFont(s.getFont().deriveFont(Font.BOLD, 12f));
-        String label = def.name;
+        String label = def.name + " - " + PlayerRegistry.name(playerId);
         int tw = s.getFontMetrics().stringWidth(label);
         s.setColor(new Color(0,0,0,160));
         s.fillRoundRect((int)(x - tw / 2.0 - 6), (int)(y - radius - 32), tw + 12, 18, 8, 8);
-        s.setColor(Color.WHITE); s.drawString(label, (int)(x - tw / 2.0), (int)(y - radius - 18));
+        s.setColor(playerColor); s.drawString(label, (int)(x - tw / 2.0), (int)(y - radius - 18));
         s.dispose();
     }
 }
