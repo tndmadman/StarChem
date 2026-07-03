a696cbd1bb9dd1a5ea2870d668ef1233bdbae461
Alic Kutia <tjmacclain98@gmail.com>
2026-07-02 23:26:19 -0600

    Fix Windows launcher for paths with spaces

--- run-starchem.bat
+++ run-starchem.bat
@@ -22,13 +22,21 @@ if errorlevel 1 (
     exit /b 1
 )
 
-if not exist build mkdir build
-if not exist build\classes mkdir build\classes
+if not exist "build" mkdir "build"
+if not exist "build\classes" mkdir "build\classes"
 
-dir /s /b src\main\java\*.java > build\sources.txt
+set MAIN_SOURCE=src\main\java\com\tndmadman\rts\RtsGame.java
+
+if not exist "%MAIN_SOURCE%" (
+    echo Could not find %MAIN_SOURCE%.
+    echo Make sure you are running this from the StarChem repo folder.
+    echo.
+    pause
+    exit /b 1
+)
 
 echo Compiling StarChem...
-javac --release 17 -encoding UTF-8 -d build\classes @build\sources.txt
+javac --release 17 -encoding UTF-8 -d "build\classes" "%MAIN_SOURCE%"
 if errorlevel 1 (
     echo.
     echo Compile failed.
@@ -38,4 +46,4 @@ if errorlevel 1 (
 
 echo Launching StarChem...
 echo.
-java -cp build\classes com.tndmadman.rts.RtsGame %*
+java -cp "build\classes" com.tndmadman.rts.RtsGame %*
