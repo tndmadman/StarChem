39c6006a28fed0b38f3ffac43c5cd1da3e00681c
Alic Kutia <tjmacclain98@gmail.com>
2026-07-03 13:47:25 -0600

    Soften local snapshot correction

--- src/main/java/com/tndmadman/rts/SnapshotSmoother.java
+++ src/main/java/com/tndmadman/rts/SnapshotSmoother.java
@@ -5,12 +5,15 @@ final class SnapshotSmoother {
 
     static void apply(Unit unit, UnitState state) {
         double error = Calc.distance(unit.x, unit.y, state.x(), state.y());
-        if (error > 180) {
+        boolean local = PlayerRegistry.isLocal(unit.playerId);
+        double snap = local ? 260 : 180;
+        double blend = local ? 0.12 : 0.32;
+        if (error > snap) {
             unit.x = state.x();
             unit.y = state.y();
         } else {
-            unit.x = Calc.lerp(unit.x, state.x(), 0.28);
-            unit.y = Calc.lerp(unit.y, state.y(), 0.28);
+            unit.x = Calc.lerp(unit.x, state.x(), blend);
+            unit.y = Calc.lerp(unit.y, state.y(), blend);
         }
         unit.targetX = state.targetX();
         unit.targetY = state.targetY();
