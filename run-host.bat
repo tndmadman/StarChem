5c55098c66d85c29ccdd041a7876494bd56439d3
Alic Kutia <tjmacclain98@gmail.com>
2026-07-02 23:34:54 -0600

    Pass host display name from Windows launcher

--- run-host.bat
+++ run-host.bat
@@ -3,11 +3,14 @@ setlocal
 cd /d "%~dp0"
 
 set PORT=50000
-if not "%~1"=="" set PORT=%~1
+set PLAYER_NAME=Host-%COMPUTERNAME%
 
-echo Starting StarChem HOST on UDP port %PORT%...
+if not "%~1"=="" set PORT=%~1
+if not "%~2"=="" set PLAYER_NAME=%~2
+
+echo Starting StarChem HOST on UDP port %PORT% as %PLAYER_NAME%...
 echo.
-call "%~dp0run-starchem.bat" --host %PORT% --id HOST
+call "%~dp0run-starchem.bat" --host %PORT% --name "%PLAYER_NAME%"
 
 echo.
 pause
