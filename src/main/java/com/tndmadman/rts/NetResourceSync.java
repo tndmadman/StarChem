be9a0480997546f420e19a89f425d47602496f73
Alic Kutia <tjmacclain98@gmail.com>
2026-07-03 14:13:30 -0600

    Avoid active resource reanchor jitter

--- src/main/java/com/tndmadman/rts/NetResourceSync.java
+++ src/main/java/com/tndmadman/rts/NetResourceSync.java
@@ -6,16 +6,19 @@ final class NetResourceSync {
     static void apply(World world, Iterable<ResourceState> states) {
         for (ResourceState s : states) {
             ResourceNode node = world.findResource(s.id());
+            boolean wasActive = node != null && node.active;
             if (node == null) {
                 node = new ResourceNode(s.id(), s.name(), NodeKind.valueOf(s.kind()), Material.valueOf(s.material()), s.x(), s.y(), s.maxAmount(), s.harvestRate(), s.radius());
                 world.resources.add(node);
             }
-            node.x = s.x();
-            node.y = s.y();
             node.amount = s.amount();
             node.active = s.active();
             node.respawnTimer = s.respawnTimer();
-            if (node.active) reanchorOrbit(world, node);
+            if (!wasActive && node.active) {
+                node.x = s.x();
+                node.y = s.y();
+                reanchorOrbit(world, node);
+            }
         }
     }
 
