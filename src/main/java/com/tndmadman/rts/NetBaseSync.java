2673f55078c4b913d53065e92e33c5e8eca42654
Alic Kutia <tjmacclain98@gmail.com>
2026-07-03 15:41:08 -0600

    Sync base HP state

--- src/main/java/com/tndmadman/rts/NetBaseSync.java
+++ src/main/java/com/tndmadman/rts/NetBaseSync.java
@@ -5,11 +5,12 @@ final class NetBaseSync {
 
     static Base fromState(BaseState state) {
         Base base = new Base(state.id(), state.playerId(), state.typeId(), state.x(), state.y());
+        base.hp = state.hp();
         CargoCodec.readInto(state.cargo(), base.inventory);
         return base;
     }
 
     static BaseState toState(Base base) {
-        return new BaseState(base.id, base.playerId, base.typeId, base.x, base.y, CargoCodec.write(base.inventory));
+        return new BaseState(base.id, base.playerId, base.typeId, base.x, base.y, base.hp, CargoCodec.write(base.inventory));
     }
 }
