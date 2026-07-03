507758b4576065ccb0131cba2a6522f466e9db4a
Alic Kutia <tjmacclain98@gmail.com>
2026-07-03 15:40:49 -0600

    Read combat fields from snapshots

--- src/main/java/com/tndmadman/rts/SnapshotReader.java
+++ src/main/java/com/tndmadman/rts/SnapshotReader.java
@@ -16,7 +16,12 @@ final class SnapshotReader {
         List<UnitState> units = new ArrayList<>();
         if (p.length > 3 && !p[3].isBlank()) for (String row : p[3].split(";")) {
             String[] c = row.split(",", -1);
-            if (c.length >= 12) units.add(new UnitState(c[0], Integer.parseInt(c[1]), c[2], Double.parseDouble(c[3]), Double.parseDouble(c[4]), Double.parseDouble(c[5]), Double.parseDouble(c[6]), Double.parseDouble(c[7]), c[8], Integer.parseInt(c[9]), CargoCodec.unsafed(c[10]), CargoCodec.unsafed(c[11])));
+            if (c.length >= 12) {
+                double hp = c.length >= 13 ? Double.parseDouble(c[12]) : Rules.ship(c[2]).maxHp;
+                String target = c.length >= 14 ? CargoCodec.unsafed(c[13]) : "";
+                double flash = c.length >= 15 ? Double.parseDouble(c[14]) : 0;
+                units.add(new UnitState(c[0], Integer.parseInt(c[1]), c[2], Double.parseDouble(c[3]), Double.parseDouble(c[4]), Double.parseDouble(c[5]), Double.parseDouble(c[6]), Double.parseDouble(c[7]), c[8], Integer.parseInt(c[9]), CargoCodec.unsafed(c[10]), CargoCodec.unsafed(c[11]), hp, target, flash));
+            }
         }
         return new Snapshot(seq, players, units, SnapshotReader2.resources(p), SnapshotReader2.bases(p), SnapshotReader2.stocks(p));
     }
