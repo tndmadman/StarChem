98c8ffa1fab8658dd7a16304d52b1382b78479e4
Alic Kutia <tjmacclain98@gmail.com>
2026-07-04 20:21:58 -0600

    Sync base shield state

--- src/main/java/com/tndmadman/rts/NetBaseSync.java
+++ src/main/java/com/tndmadman/rts/NetBaseSync.java
@@ -6,11 +6,12 @@ final class NetBaseSync {
     static Base fromState(BaseState state) {
         Base base = new Base(state.id(), state.playerId(), state.typeId(), state.x(), state.y());
         base.hp = state.hp();
+        base.shield = state.shield();
         CargoCodec.readInto(state.cargo(), base.inventory);
         return base;
     }
 
     static BaseState toState(Base base) {
-        return new BaseState(base.id, base.playerId, base.typeId, base.x, base.y, base.hp, CargoCodec.write(base.inventory));
+        return new BaseState(base.id, base.playerId, base.typeId, base.x, base.y, base.hp, base.shield, CargoCodec.write(base.inventory));
     }
 }
