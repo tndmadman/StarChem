8276c6f17e0c11a8fdf3f02ab971026bdbc8503b
Alic Kutia <tjmacclain98@gmail.com>
2026-07-04 20:19:19 -0600

    Sync base state parser

--- src/main/java/com/tndmadman/rts/BaseStateParser.java
+++ src/main/java/com/tndmadman/rts/BaseStateParser.java
@@ -7,7 +7,8 @@ final class BaseStateParser {
         String[] c = row.split(",", -1);
         if (c.length < 5) return null;
         double hp = c.length >= 7 ? Double.parseDouble(c[5]) : Rules.base(c[2]).maxHp;
-        String cargo = c.length >= 7 ? CargoCodec.unsafed(c[6]) : c.length >= 6 ? CargoCodec.unsafed(c[5]) : "";
-        return new BaseState(c[0], c[1], c[2], Double.parseDouble(c[3]), Double.parseDouble(c[4]), hp, cargo);
+        double shield = c.length >= 8 ? Double.parseDouble(c[6]) : Rules.base(c[2]).maxShield;
+        String cargo = c.length >= 8 ? CargoCodec.unsaf\u0065d(c[7]) : c.length >= 7 ? CargoCodec.unsaf\u0065d(c[6]) : c.length >= 6 ? CargoCodec.unsaf\u0065d(c[5]) : "";
+        return new BaseState(c[0], c[1], c[2], Double.parseDouble(c[3]), Double.parseDouble(c[4]), hp, shield, cargo);
     }
 }
