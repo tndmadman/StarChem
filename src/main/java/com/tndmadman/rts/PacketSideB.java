a0d00f4cb75957b4a795cee14504d5b179ccb252
Alic Kutia <tjmacclain98@gmail.com>
2026-07-07 15:16:08 -0600

    Move client packet routing out of PeerNetwork

--- src/main/java/com/tndmadman/rts/PacketSideB.java
+++ src/main/java/com/tndmadman/rts/PacketSideB.java
@@ -4,6 +4,10 @@ final class PacketSideB {
     private PacketSideB() { }
 
     static void handle(PeerNetwork net, String message) {
-        net.handleClientPacket(message);
+        String[] p = message.split("\\|", -1);
+        if (p[0].equals("ENV")) { net.readEnv(p); return; }
+        if (p[0].equals("SEED") && p.length >= 2) { net.readSeed(p[1]); return; }
+        if (p[0].equals("WELCOME")) { net.readWelcome(p); return; }
+        if (p[0].equals("SNAPSHOT")) net.readSnapshot(message);
     }
 }
