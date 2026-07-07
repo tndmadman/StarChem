c06077e53fac7b0d9007649925bce597804f90b7
Alic Kutia <tjmacclain98@gmail.com>
2026-07-07 16:12:55 -0600

    Use endpoint owner for server orders

--- src/main/java/com/tndmadman/rts/AUnitMove.java
+++ src/main/java/com/tndmadman/rts/AUnitMove.java
@@ -13,15 +13,16 @@ final class SideAOrders {
     private SideAOrders() { }
 
     static void handle(PeerServerSide s, String[] p, String ep) {
+        String id = p.length > 1 ? s.ownerId(ep, p[1]) : "";
         switch (p[0]) {
-            case "MOVE" -> { s.touch(ep); if (s.owns(ep, p[1])) s.change(p[1], () -> AUnitMove.apply(s.world, new MoveCommand(p[1], Integer.parseInt(p[2]), Double.parseDouble(p[3]), Double.parseDouble(p[4])))); }
-            case "WORK" -> { s.touch(ep); if (s.owns(ep, p[1])) s.change(p[1], () -> AUnitWork.apply(s.world, new HarvestCommand(p[1], Integer.parseInt(p[2]), Integer.parseInt(p[3])))); }
-            case "ATTACK" -> { s.touch(ep); if (s.owns(ep, p[1])) s.change(p[1], () -> AUnitAttack.apply(s.world, new AttackCommand(p[1], Integer.parseInt(p[2]), p[3]))); }
-            case "RESPAWN" -> { s.touch(ep); if (s.owns(ep, p[1])) { s.change(p[1], () -> WorldNetAccess.respawnPlayer(s.world, p[1])); s.broadcastNow(); } }
-            case "BUILD" -> { s.touch(ep); if (s.owns(ep, p[1])) s.change(p[1], () -> { if (CommandAuth.base(s.world, p[1], p[2])) s.world.buildShip(p[2], p[3]); }); }
-            case "PACK" -> { s.touch(ep); if (s.owns(ep, p[1])) s.change(p[1], () -> { if (CommandAuth.pack(s.world, p[1], p[2], p[3])) AUnitPack.apply(s.world, p[2], p[3], p[4]); }); }
-            case "JUMP" -> { s.touch(ep); if (s.owns(ep, p[1])) { s.change(p[1], () -> s.world.jumpThroughWormholeAt(Double.parseDouble(p[2]), Double.parseDouble(p[3]))); s.sendInitialTo(ep); } }
-            case "WHTOUCH" -> { s.touch(ep); if (s.owns(ep, p[1])) { s.change(p[1], s.world::transferTouchingShips); s.sendInitialTo(ep); } }
+            case "MOVE" -> { s.touch(ep); if (s.owns(ep, id)) s.change(id, () -> AUnitMove.apply(s.world, new MoveCommand(id, Integer.parseInt(p[2]), Double.parseDouble(p[3]), Double.parseDouble(p[4])))); }
+            case "WORK" -> { s.touch(ep); if (s.owns(ep, id)) s.change(id, () -> AUnitWork.apply(s.world, new HarvestCommand(id, Integer.parseInt(p[2]), Integer.parseInt(p[3])))); }
+            case "ATTACK" -> { s.touch(ep); if (s.owns(ep, id)) s.change(id, () -> AUnitAttack.apply(s.world, new AttackCommand(id, Integer.parseInt(p[2]), p[3]))); }
+            case "RESPAWN" -> { s.touch(ep); if (s.owns(ep, id)) { s.change(id, () -> WorldNetAccess.respawnPlayer(s.world, id)); s.broadcastNow(); } }
+            case "BUILD" -> { s.touch(ep); if (s.owns(ep, id)) s.change(id, () -> { if (CommandAuth.base(s.world, id, p[2])) s.world.buildShip(p[2], p[3]); }); }
+            case "PACK" -> { s.touch(ep); if (s.owns(ep, id)) s.change(id, () -> { if (CommandAuth.pack(s.world, id, p[2], p[3])) AUnitPack.apply(s.world, p[2], p[3], p[4]); }); }
+            case "JUMP" -> { s.touch(ep); if (s.owns(ep, id)) { s.change(id, () -> s.world.jumpThroughWormholeAt(Double.parseDouble(p[2]), Double.parseDouble(p[3]))); s.sendInitialTo(ep); } }
+            case "WHTOUCH" -> { s.touch(ep); if (s.owns(ep, id)) { s.change(id, s.world::transferTouchingShips); s.sendInitialTo(ep); } }
         }
     }
 }
