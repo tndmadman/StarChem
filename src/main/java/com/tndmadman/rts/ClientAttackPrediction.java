32ee8bf1dd39c937757e38e9520af4ad6c226867
Alic Kutia <tjmacclain98@gmail.com>
2026-07-07 15:54:39 -0600

    Add client attack prediction helper

---
+++ src/main/java/com/tndmadman/rts/ClientAttackPrediction.java
@@ -0,0 +1,15 @@
+package com.tndmadman.rts;
+
+final class ClientAttackPrediction {
+    private ClientAttackPrediction() { }
+
+    static void apply(World world, Unit unit) {
+        if (unit.task != UnitTask.ATTACK) return;
+        if (!CombatTarget.enemy(world, unit, unit.attackTarget)) return;
+        double range = WeaponRules.maxRange(unit.type());
+        if (range <= 0) return;
+        double tx = CombatTarget.x(world, unit.attackTarget);
+        double ty = CombatTarget.y(world, unit.attackTarget);
+        if (Calc.distance(unit.x, unit.y, tx, ty) > range * 0.92) world.moveTowardOrbit(unit, tx, ty, range * 0.82);
+    }
+}
