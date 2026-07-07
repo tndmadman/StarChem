e9784718baa3bb2a5080d2265d9cecd4d708eb63
Alic Kutia <tjmacclain98@gmail.com>
2026-07-07 15:51:18 -0600

    Force full resources for initial sync

--- src/main/java/com/tndmadman/rts/SyncPacketBuilder.java
+++ src/main/java/com/tndmadman/rts/SyncPacketBuilder.java
@@ -4,6 +4,7 @@ final class SyncPacketBuilder {
     private SyncPacketBuilder() { }
 
     static String build(World world, ClientViewCache views, String playerId, long sequence, SyncKind kind) {
+        if (kind == SyncKind.INITIAL) ResourceSyncMode.fullForNextSnapshot();
         Snapshot snapshot = views.makeSnapshot(world, playerId, sequence);
         return kind == SyncKind.INITIAL ? SyncFrame.write(snapshot) : SnapshotWriter.write(snapshot);
     }
