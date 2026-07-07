036bfcd013af7b8567edd170961acafce27fad2d
Alic Kutia <tjmacclain98@gmail.com>
2026-07-07 00:45:38 -0600

    Create missing view resources

--- src/main/java/com/tndmadman/rts/ResourceViewSync.java
+++ src/main/java/com/tndmadman/rts/ResourceViewSync.java
@@ -6,7 +6,10 @@ final class ResourceViewSync {
     static void apply(World world, Iterable<ResourceState> states) {
         for (ResourceState s : states) {
             ResourceNode node = world.findResource(s.id());
-            if (node == null) continue;
+            if (node == null) {
+                node = new ResourceNode(s.id(), s.name(), NodeKind.valueOf(s.kind()), Material.valueOf(s.material()), s.x(), s.y(), s.maxAmount(), s.harvestRate(), s.radius());
+                world.resources.add(node);
+            }
             node.x = s.x();
             node.y = s.y();
             node.amount = s.amount();
