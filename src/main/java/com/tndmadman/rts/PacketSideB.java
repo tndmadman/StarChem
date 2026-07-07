4f82910229fe9d41fbbdc705e005ac14b02b3b16
Alic Kutia <tjmacclain98@gmail.com>
2026-07-07 15:19:13 -0600

    Minimize second packet helper

--- src/main/java/com/tndmadman/rts/PacketSideB.java
+++ src/main/java/com/tndmadman/rts/PacketSideB.java
@@ -4,10 +4,5 @@ final class PacketSideB {
     private PacketSideB() { }
 
     static void handle(PeerNetwork net, String message) {
-        String[] p = message.split("\\|", -1);
-        if (p[0].equals("ENV")) { net.readEnv(p); return; }
-        if (p[0].equals("SEED") && p.length >= 2) { net.readSeed(p[1]); return; }
-        if (p[0].equals("WELCOME")) { net.readWelcome(p); return; }
-        if (p[0].equals("SNAPSHOT")) net.readSnapshot(message);
     }
 }
