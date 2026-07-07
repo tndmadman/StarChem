257c8333b0436d4ff1278eba9504620591183edf
Alic Kutia <tjmacclain98@gmail.com>
2026-07-07 14:57:28 -0600

    Add headless server starter

---
+++ src/main/java/com/tndmadman/rts/HeadlessGameServer.java
@@ -0,0 +1,19 @@
+package com.tndmadman.rts;
+
+import java.io.IOException;
+
+final class HeadlessGameServer {
+    final World world;
+    final PeerNetwork network;
+
+    private HeadlessGameServer(World world, PeerNetwork network) {
+        this.world = world;
+        this.network = network;
+    }
+
+    static HeadlessGameServer start(Config config) throws IOException {
+        World world = new World(config.playerName, config.disabledNpcFactionIds, config.systemId);
+        PeerNetwork network = PeerNetwork.start(config, world);
+        return new HeadlessGameServer(world, network);
+    }
+}
