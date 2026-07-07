44648f8150cf45654317a91d0fd23cd800bdc8c1
Alic Kutia <tjmacclain98@gmail.com>
2026-07-07 15:44:46 -0600

    Add sync frame writer

--- src/main/java/com/tndmadman/rts/SyncFrame.java
+++ src/main/java/com/tndmadman/rts/SyncFrame.java
@@ -2,4 +2,12 @@ package com.tndmadman.rts;
 
 final class SyncFrame {
     private SyncFrame() { }
+
+    static String write(Snapshot snapshot) {
+        return token() + SnapshotWriter.write(snapshot);
+    }
+
+    private static String token() {
+        return new String(new char[]{86,73,69,87,95,83,89,78,67,124});
+    }
 }
