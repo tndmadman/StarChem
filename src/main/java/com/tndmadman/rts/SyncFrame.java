344426d77b028ee6ac037cfc977ee09e6431bb82
Alic Kutia <tjmacclain98@gmail.com>
2026-07-07 15:45:01 -0600

    Add sync frame reader

--- src/main/java/com/tndmadman/rts/SyncFrame.java
+++ src/main/java/com/tndmadman/rts/SyncFrame.java
@@ -7,6 +7,15 @@ final class SyncFrame {
         return token() + SnapshotWriter.write(snapshot);
     }
 
+    static Snapshot read(String message) {
+        String prefix = token();
+        return message.startsWith(prefix) ? SnapshotReader.read(message.substring(prefix.length())) : SnapshotReader.read(message);
+    }
+
+    static boolean matches(String message) {
+        return message.startsWith(token());
+    }
+
     private static String token() {
         return new String(new char[]{86,73,69,87,95,83,89,78,67,124});
     }
