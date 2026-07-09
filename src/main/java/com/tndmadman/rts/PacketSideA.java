c1033e411603ea689de79c6a49fc34d88482df09
Alic Kutia <tjmacclain98@gmail.com>
2026-07-09 11:34:09 -0600

    Add server-authorized client dev packets

--- src/main/java/com/tndmadman/rts/PacketSideA.java
+++ src/main/java/com/tndmadman/rts/PacketSideA.java
@@ -8,6 +8,7 @@ final class PacketSideA {
         String ep = s.endpoint(packet.address(), packet.port());
         try {
             if (SideAJoin.handle(s, p, ep, packet)) return;
+            if (SideADev.handle(s, p, ep)) return;
             SideAOrders.handle(s, p, ep);
         } catch (Exception ex) {
             System.err.println("Bad packet: " + message + " / " + ex.getMessage());
