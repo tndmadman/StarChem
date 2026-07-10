c745a05581a7e25fe70615abdd69d45007424680
Alic Kutia <tjmacclain98@gmail.com>
2026-07-10 11:42:45 -0600

    Add incoming wormhole alarm

--- src/main/java/com/tndmadman/rts/WormholeTransitNotice.java
+++ src/main/java/com/tndmadman/rts/WormholeTransitNotice.java
@@ -2,6 +2,10 @@ package com.tndmadman.rts;
 
 final class WormholeTransitNotice {
     private static final long EXIT_DELAY_MS = 260;
+    private static final long ALARM_SECOND_TONE_DELAY_MS = 150;
+    private static final long INCOMING_ALARM_COOLDOWN_NANOS = 900_000_000L;
+    private static String lastIncomingSystemId = "";
+    private static long lastIncomingAlarmNanos;
 
     private WormholeTransitNotice() { }
 
@@ -15,6 +19,29 @@ final class WormholeTransitNotice {
 
     static void play(World world, String systemId) {
         play(world, systemId, systemId);
+    }
+
+    static void incoming(String destinationSystemId) {
+        if (!SystemAudio.audible(destinationSystemId)) return;
+        long now = System.nanoTime();
+        synchronized (WormholeTransitNotice.class) {
+            if (destinationSystemId.equals(lastIncomingSystemId)
+                    && now - lastIncomingAlarmNanos < INCOMING_ALARM_COOLDOWN_NANOS) return;
+            lastIncomingSystemId = destinationSystemId;
+            lastIncomingAlarmNanos = now;
+        }
+        SystemAudio.play(destinationSystemId, SoundCue.ATTACK_ORDER);
+        Thread secondTone = new Thread(() -> {
+            try {
+                Thread.sleep(ALARM_SECOND_TONE_DELAY_MS);
+            } catch (InterruptedException ignored) {
+                Thread.currentThread().interrupt();
+                return;
+            }
+            SystemAudio.play(destinationSystemId, SoundCue.ERROR);
+        }, "StarChem Incoming Wormhole Alarm");
+        secondTone.setDaemon(true);
+        secondTone.start();
     }
 
     private static void play(World world, String entrySystemId, String exitSystemId) {
