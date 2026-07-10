8a0142052c42ff1593f69b22c108e96d62443ab3
Alic Kutia <tjmacclain98@gmail.com>
2026-07-10 12:22:38 -0600

    Read production queues from snapshots

--- src/main/java/com/tndmadman/rts/BaseStateParser.java
+++ src/main/java/com/tndmadman/rts/BaseStateParser.java
@@ -8,7 +8,8 @@ final class BaseStateParser {
         if (c.length < 5) return null;
         double hp = c.length >= 7 ? Double.parseDouble(c[5]) : Rules.base(c[2]).maxHp;
         double shield = c.length >= 8 ? Double.parseDouble(c[6]) : Rules.base(c[2]).maxShield;
-        String cargo = c.length >= 8 ? CargoCodec.unsaf\u0065d(c[7]) : c.length >= 7 ? CargoCodec.unsaf\u0065d(c[6]) : c.length >= 6 ? CargoCodec.unsaf\u0065d(c[5]) : "";
-        return new BaseState(c[0], c[1], c[2], Double.parseDouble(c[3]), Double.parseDouble(c[4]), hp, shield, cargo);
+        String cargo = c.length >= 8 ? CargoCodec.unsafed(c[7]) : c.length >= 7 ? CargoCodec.unsafed(c[6]) : c.length >= 6 ? CargoCodec.unsafed(c[5]) : "";
+        String productionQueue = c.length >= 9 ? CargoCodec.unsafed(c[8]) : "";
+        return new BaseState(c[0], c[1], c[2], Double.parseDouble(c[3]), Double.parseDouble(c[4]), hp, shield, cargo, productionQueue);
     }
 }
