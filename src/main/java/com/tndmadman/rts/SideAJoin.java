7cc07f3192a7c9640051f78590fd77143dc9764a
Alic Kutia <tjmacclain98@gmail.com>
2026-07-10 22:45:06 -0600

    Pass remote dev credentials through join

--- src/main/java/com/tndmadman/rts/SideAJoin.java
+++ src/main/java/com/tndmadman/rts/SideAJoin.java
@@ -5,7 +5,11 @@ final class SideAJoin {
 
     static boolean handle(PeerServerSide s, String[] p, String ep, NetPacket packet) {
         switch (p[0]) {
-            case "JOIN" -> { s.join(ep, packet.address(), packet.port(), p.length > 1 ? p[1] : "Player", s.requestedDev(p)); return true; }
+            case "JOIN" -> {
+                s.join(ep, packet.address(), packet.port(), p.length > 1 ? p[1] : "Player",
+                        s.requestedDev(p), s.requestedDevToken(p));
+                return true;
+            }
             case "PING" -> { s.touch(ep); return true; }
             case "LEAVE" -> { s.removePeer(ep); return true; }
             default -> { return false; }
