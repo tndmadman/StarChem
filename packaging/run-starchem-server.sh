6e8090994ac139b085ab270ded424d2fe5011c60
Alic Kutia <tjmacclain98@gmail.com>
2026-07-12 12:17:33 -0600

    Add Linux dedicated server launcher

---
+++ packaging/run-starchem-server.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+set -eu
+
+SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+cd "$SCRIPT_DIR"
+
+PORT=${STARCHEM_PORT:-50000}
+SERVER_NAME=${STARCHEM_SERVER_NAME:-StarChem-Server}
+
+exec "${JAVA_BIN:-java}" -Djava.awt.headless=true -jar StarChem.jar \
+    --server "$PORT" \
+    --name "$SERVER_NAME" \
+    "$@"
