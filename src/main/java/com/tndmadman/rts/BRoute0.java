2759ffa44faaf0027b0b1581fe85188224470b9b
Alic Kutia <tjmacclain98@gmail.com>
2026-07-13 13:41:58 -0600

    Route server notices to client narration

--- src/main/java/com/tndmadman/rts/BRoute0.java
+++ src/main/java/com/tndmadman/rts/BRoute0.java
@@ -4,6 +4,9 @@ final class BRoute0 {
     private BRoute0() { }
 
     static boolean apply(PeerClientSide c, String message) {
+        if (message != null && message.startsWith("NOTICE|")) {
+            return GameNoticeCenter.acceptRemote(c.world, message);
+        }
         if (!SyncFrame.matches(message)) return false;
         c.readFullView(message);
         return true;
