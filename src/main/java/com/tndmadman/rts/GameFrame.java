a4aa6d08639e971928ea184bccd19d85235667fa
Alic Kutia <tjmacclain98@gmail.com>
2026-07-14 13:51:50 -0600

    Allow typing in resource catalog search

--- src/main/java/com/tndmadman/rts/GameFrame.java
+++ src/main/java/com/tndmadman/rts/GameFrame.java
@@ -142,6 +142,7 @@ final class GameFrame extends JFrame {
         target.getActionMap().put(RESOURCE_CATALOG_ACTION, new AbstractAction() {
             @Override public void actionPerformed(ActionEvent e) {
                 if (resourceCatalogOverlay == null) return;
+                if (resourceCatalogOverlay.isSearchFocused()) return;
                 if (codexOverlay != null && codexOverlay.isVisible()) codexOverlay.close();
                 if (narrationSettingsOverlay != null && narrationSettingsOverlay.isVisible()) narrationSettingsOverlay.close();
                 resourceCatalogOverlay.toggle();
@@ -155,7 +156,7 @@ final class GameFrame extends JFrame {
         target.getActionMap().put(CODEX_ACTION, new AbstractAction() {
             @Override public void actionPerformed(ActionEvent e) {
                 if (codexOverlay == null) return;
-                if (resourceCatalogOverlay != null && resourceCatalogOverlay.isVisible()) resourceCatalogOverlay.setVisible(false);
+                if (resourceCatalogOverlay != null && resourceCatalogOverlay.isVisible()) resourceCatalogOverlay.close();
                 if (narrationSettingsOverlay != null && narrationSettingsOverlay.isVisible()) narrationSettingsOverlay.close();
                 codexOverlay.toggle();
             }
@@ -168,7 +169,7 @@ final class GameFrame extends JFrame {
         target.getActionMap().put(NARRATION_SETTINGS_ACTION, new AbstractAction() {
             @Override public void actionPerformed(ActionEvent e) {
                 if (narrationSettingsOverlay == null) return;
-                if (resourceCatalogOverlay != null && resourceCatalogOverlay.isVisible()) resourceCatalogOverlay.setVisible(false);
+                if (resourceCatalogOverlay != null && resourceCatalogOverlay.isVisible()) resourceCatalogOverlay.close();
                 if (codexOverlay != null && codexOverlay.isVisible()) codexOverlay.close();
                 narrationSettingsOverlay.toggle();
             }
