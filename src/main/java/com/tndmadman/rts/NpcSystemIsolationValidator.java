6b37b27a7c06b09a10b2472288a5371645cd9d94
Alic Kutia <tjmacclain98@gmail.com>
2026-07-15 13:49:14 -0600

    wire npc recovery validation

--- src/main/java/com/tndmadman/rts/NpcSystemIsolationValidator.java
+++ src/main/java/com/tndmadman/rts/NpcSystemIsolationValidator.java
@@ -24,6 +24,7 @@ public final class NpcSystemIsolationValidator {
         NpcResourceBudgetValidator.validateOrThrow();
         NpcResourceBudgetCapabilityValidator.validateOrThrow();
         NpcWorkerProductionValidator.validateOrThrow();
+        NpcRecoveryValidator.validateOrThrow();
         validateRuntimeCleanup();
     }
 
