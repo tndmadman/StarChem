ccfff4d6c02dde682d1da6c3b9da716ffb7c3be6
Alic Kutia <tjmacclain98@gmail.com>
2026-07-15 21:14:09 -0600

    wire npc station construction validation

--- src/main/java/com/tndmadman/rts/NpcSystemIsolationValidator.java
+++ src/main/java/com/tndmadman/rts/NpcSystemIsolationValidator.java
@@ -24,6 +24,7 @@ public final class NpcSystemIsolationValidator {
         NpcResourceBudgetValidator.validateOrThrow();
         NpcResourceBudgetCapabilityValidator.validateOrThrow();
         NpcWorkerProductionValidator.validateOrThrow();
+        NpcStationConstructionValidator.validateOrThrow();
         NpcRecoveryValidator.validateOrThrow();
         validateRuntimeCleanup();
     }
