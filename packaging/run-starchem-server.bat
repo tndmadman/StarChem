b286cbdc6a4e381039c566c84d7673875efd700b
Alic Kutia <tjmacclain98@gmail.com>
2026-07-22 14:45:50 -0600

    Support protected token files in Windows launcher

--- packaging/run-starchem-server.bat
+++ packaging/run-starchem-server.bat
@@ -3,5 +3,9 @@ setlocal
 cd /d "%~dp0"
 if not defined STARCHEM_PORT set "STARCHEM_PORT=50000"
 if not defined STARCHEM_SERVER_NAME set "STARCHEM_SERVER_NAME=StarChem-Server"
-java -Djava.awt.headless=true -jar "StarChem.jar" --server "%STARCHEM_PORT%" --name "%STARCHEM_SERVER_NAME%" %*
+if defined STARCHEM_DEV_TOKEN_FILE (
+    java -Djava.awt.headless=true -jar "StarChem.jar" --server "%STARCHEM_PORT%" --name "%STARCHEM_SERVER_NAME%" --dev-token-file "%STARCHEM_DEV_TOKEN_FILE%" %*
+) else (
+    java -Djava.awt.headless=true -jar "StarChem.jar" --server "%STARCHEM_PORT%" --name "%STARCHEM_SERVER_NAME%" %*
+)
 exit /b %ERRORLEVEL%
