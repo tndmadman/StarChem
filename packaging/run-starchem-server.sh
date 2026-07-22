b810f279dbf3e6687787d0e3335ce94fbe100c5b
Alic Kutia <tjmacclain98@gmail.com>
2026-07-22 14:45:44 -0600

    Support protected token files in Linux launcher

--- packaging/run-starchem-server.sh
+++ packaging/run-starchem-server.sh
@@ -7,6 +7,14 @@ cd "$SCRIPT_DIR"
 PORT=${STARCHEM_PORT:-50000}
 SERVER_NAME=${STARCHEM_SERVER_NAME:-StarChem-Server}
 
+if [ -n "${STARCHEM_DEV_TOKEN_FILE:-}" ]; then
+    exec "${JAVA_BIN:-java}" -Djava.awt.headless=true -jar StarChem.jar \
+        --server "$PORT" \
+        --name "$SERVER_NAME" \
+        --dev-token-file "$STARCHEM_DEV_TOKEN_FILE" \
+        "$@"
+fi
+
 exec "${JAVA_BIN:-java}" -Djava.awt.headless=true -jar StarChem.jar \
     --server "$PORT" \
     --name "$SERVER_NAME" \
