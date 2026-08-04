9875327bc77ef07bb45a2133d711486249a9e379
Alic Kutia <tjmacclain98@gmail.com>
2026-08-03 22:21:21 -0600

    Use authoritative attack ranges in client prediction

--- src/main/java/com/tndmadman/rts/ClientPrediction.java
+++ src/main/java/com/tndmadman/rts/ClientPrediction.java
@@ -8,7 +8,7 @@ final class ClientPrediction {
     static void update(World world, double dt) {
         for (Unit unit : world.units.values()) {
             unit.wormholeCooldown = Math.max(0, unit.wormholeCooldown - dt);
-            if (PlayerRegistry.isLocal(unit.playerId)) predictTarget(world, unit, dt);
+            if (PlayerRegistry.isLocal(unit.playerId)) AttackRangeRules.predictAttackMovement(world, unit);
             ShipModuleRules.update(world, unit, dt);
             unit.updatePosition(dt * SystemModifierRules.movementSpeed(world), world.width, world.height);
         }
@@ -18,15 +18,5 @@ final class ClientPrediction {
     private static void updateExplosions(World world, double dt) {
         Iterator<ExplosionEffect> it = world.explosions.iterator();
         while (it.hasNext()) if (!it.next().update(dt)) it.remove();
-    }
-
-    private static void predictTarget(World world, Unit unit, double dt) {
-        if (unit.task != UnitTask.ATTACK) return;
-        if (!CombatTarget.enemy(world, unit, unit.attackTarget)) return;
-        double range = WeaponRules.maxRange(unit);
-        if (range <= 0) return;
-        double tx = CombatTarget.x(world, unit.attackTarget);
-        double ty = CombatTarget.y(world, unit.attackTarget);
-        if (Calc.distance(unit.x, unit.y, tx, ty) > range * 0.92) world.moveTowardOrbit(unit, tx, ty, range * 0.82);
     }
 }
