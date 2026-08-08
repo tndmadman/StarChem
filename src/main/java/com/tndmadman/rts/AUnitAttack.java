119ffe68e955008353dbdad806da9a7951b727fc
Alic Kutia <tjmacclain98@gmail.com>
2026-08-08 12:47:52 -0600

    Release logistics routes on manual attack orders

--- src/main/java/com/tndmadman/rts/AUnitAttack.java
+++ src/main/java/com/tndmadman/rts/AUnitAttack.java
@@ -9,6 +9,7 @@ final class AUnitAttack {
         if (u == null || ProductionSystem.refitReserved(world, u.key())
                 || !VisibilityRules.targetVisible(world, c.playerId(), c.targetKey())
                 || !CombatTarget.enemy(world, u, c.targetKey()) || !WeaponRules.armed(u)) return false;
+        LogisticsRouteSystem.releaseForManualCommand(world, u.key());
         UnitCommandQueueSystem.legacyReplace(world, u);
         CombatPolicySystem.markExplicitAttack(world, u);
         u.issueAttack(c.targetKey());
