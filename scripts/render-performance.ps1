021bf8f8ca5c6789a982ab857702723399a0b102
Alic Kutia <tjmacclain98@gmail.com>
2026-09-09 14:13:32 -0600

    Run render validator through Gradle runtime classpath

--- scripts/render-performance.ps1
+++ scripts/render-performance.ps1
@@ -1,9 +1,17 @@
 $ErrorActionPreference = 'Stop'
 
-& .\gradlew.bat classes
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+$gradleArgs = @('-I', 'gradle/render-performance.gradle', 'validateRenderPerformance', '--no-daemon')
+foreach ($arg in $args) {
+    if ($arg -eq '--enforce-timing') {
+        $gradleArgs += '-PrenderPerfEnforceTiming=true'
+    } elseif ($arg.StartsWith('--baseline=')) {
+        $gradleArgs += '-PrenderPerfBaseline=' + $arg.Substring('--baseline='.Length)
+    } elseif ($arg.StartsWith('--max-regression=')) {
+        $gradleArgs += '-PrenderPerfMaxRegression=' + $arg.Substring('--max-regression='.Length)
+    } else {
+        throw "Unknown render-performance argument: $arg"
+    }
+}
 
-$classPath = 'build\classes\java\main;build\resources\main'
-$validatorArgs = @('--output=build\reports\render-performance.csv') + $args
-& java '-Djava.awt.headless=true' -cp $classPath 'com.tndmadman.rts.RenderPerformanceValidator' @validatorArgs
+& .\gradlew.bat @gradleArgs
 exit $LASTEXITCODE
