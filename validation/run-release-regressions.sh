2f5cbe5952085c3a02d7ae6cd68df65b45b664aa
Alic Kutia <tjmacclain98@gmail.com>
2026-09-09 14:49:11 -0600

    Add issue 391 background validation to release regressions

--- validation/run-release-regressions.sh
+++ validation/run-release-regressions.sh
@@ -33,6 +33,7 @@ run_java com.tndmadman.rts.ProductionLogisticsSourcingValidator
 run_java com.tndmadman.rts.ShipyardStationPackageValidator
 run_java com.tndmadman.rts.MenuOverflowValidator
 run_java com.tndmadman.rts.ShipyardScrollHotfixValidator
+run_java com.tndmadman.rts.Issue391SpaceBackgroundValidator
 run_java com.tndmadman.rts.SessionEndpointIdentityValidator
 run_java com.tndmadman.rts.NumericCommandValidationValidator authenticated
 run_java com.tndmadman.rts.NumericCommandValidationValidator repair
